0eb99ac9bba0dac2be3b9b2b0d64ac576c3f6204
ikircik <1k1rc1k@proton.me>
2023-06-21 17:46:17 +0300

    feat(ref_impl): add Add impl, AddAssign impl and direct_sum method for Matrix

--- ref_impl/src/lib.rs
+++ ref_impl/src/lib.rs
@@ -1,3 +1,5 @@
+use std::ops::{Add, AddAssign};
+
 pub struct Matrix {
     row_count: usize,
     column_count: usize,
@@ -28,6 +30,31 @@ impl Matrix {
         }
         Err(())
     }
+
+    pub fn direct_sum(&self, other: Matrix) -> Matrix {
+        let row_count = self.row_count + other.row_count;
+        let column_count = self.column_count + other.column_count;
+        let mut matrix = Matrix {
+            row_count,
+            column_count,
+            elements: vec![0.0; row_count * column_count],
+        };
+
+        for i in 0..self.row_count {
+            for j in 0..self.column_count {
+                matrix.elements[i * column_count + j] = self.elements[i * self.column_count + j];
+            }
+        }
+
+        for i in 0..other.row_count {
+            for j in 0..other.column_count {
+                matrix.elements[self.row_count * column_count + self.column_count * (i*2+1) + j]
+                = other.elements[i * other.column_count + j];
+            }
+        }
+
+        matrix
+    }
 }
 
 impl PartialEq for Matrix {
@@ -35,5 +62,30 @@ impl PartialEq for Matrix {
         self.row_count == other.row_count &&
         self.column_count == other.column_count &&
         self.elements == other.elements
+    }
+}
+
+impl Add for Matrix {
+    type Output = Result<Matrix, ()>;
+    fn add(self, other: Self) -> Self::Output {
+        if &self.row_count != &other.row_count || &self.column_count != &other.column_count {
+            return Err(());
+        }
+        let mut elements = vec![0.0; self.elements.len()];
+        for i in 0..self.elements.len() {
+            elements[i] = &self.elements[i] + &other.elements[i];
+        }
+        Ok(Matrix { row_count: self.row_count, column_count: self.column_count, elements })
+    }
+}
+
+impl AddAssign for Matrix {
+    fn add_assign(&mut self, other: Self) {
+        if &self.row_count != &other.row_count || &self.column_count != &other.column_count {
+            panic!()
+        }
+        for i in 0..self.elements.len() {
+            self.elements[i] = &self.elements[i] + &other.elements[i];
+        }
     }
 }
